85269c9ae0ffc1461a143087f48ef967fc03146c
Thiago Jatobá <tmjatob@gmail.com>
2021-02-28 20:52:31 -0300

    Second Commit/Blackjack challenges unsolved

--- semana3/blackjack/Blackjack/desafio.js
+++ semana3/blackjack/Blackjack/desafio.js
@@ -11,3 +11,47 @@
  * 
  */
 
+ //VARIAVEIS
+const carta = comprarCarta()
+let pontoMaquina = 0
+let pontoUsuario = 0
+
+let jogoMaquina;
+let jogoUsuario;
+
+let cartasUsuario = []
+let cartasMaquina = []
+
+let valorMenor21 = true
+
+
+//JOGANDO
+if(confirm("Quer iniciar uma nova rodada?")) {
+
+   while (valorMenor21 === true ){
+
+      jogoUsuario = comprarCarta()
+      console.log(carta.texto)
+      console.log(carta.valor)
+
+      jogoMaquina = comprarCarta()
+      console.log(carta.texto)
+      console.log(carta.valor)
+
+
+      if (jogoUsuario.valor < 21 || jogoMaquina.valor < 21 ){ 
+      
+      pontoUsuario += jogoUsuario.valor
+      cartasUsuario.push(jogoUsuario.texto)
+
+      pontoMaquina += jogoMaquina.valor
+      cartasMaquina.push(jogoMaquina.valor)
+
+      console.log(`Carta CPU: ${cartasMaquina}, Sua Carta: ${cartasUsuario}`)
+      console.log(`Pontos CPU: ${pontoMaquina}, Seus pontos: ${pontoUsuario}`)
+      } else { 
+         valorMenor21 = false
+      } 
+      console.log (valorMenor21) 
+   }
+}
