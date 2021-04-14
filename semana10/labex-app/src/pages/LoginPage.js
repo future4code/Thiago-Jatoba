1b29cc71c0043ba4a45287e17dabeec9afd14036
Thiago Jatobá <tmjatob@gmail.com>
2021-04-14 18:50:27 -0300

    [feat] auteticação aplicada

--- semana10/labex-app/src/pages/LoginPage.js
+++ semana10/labex-app/src/pages/LoginPage.js
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import { useHistory } from "react-router";
+import axios from 'axios'
 import {goToHomePage, goToAdminHomePage} from '../routes/coordinator'
 
 function LoginPage() {
     const history = useHistory()
+    const [usermail, setUserMail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleMail = (e) => {
+        setUserMail(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const login = async () => {
+        const body = {
+            email: usermail,
+            password: password
+        }
+        try{
+            const res = await axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/login', body)
+            console.log(res.data)
+            goToAdminHomePage(history)
+            window.localStorage.setItem('token', res.data.token)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
 
     return (
         <div>
             LogIn
+            <input value={usermail} onChange= {handleMail} placeholder='email'/>
+            <input value={password} onChange= {handlePassword} placeholder='senha'/>
             <button onClick={() => goToHomePage(history)}>Voltar</button>
-            <button onClick={() => goToAdminHomePage(history)}>Entrar</button>
+            <button onClick={login} >Entrar</button>
         </div>
     );
 }
--- semana10/labex-app/src/pages/TripDetailPage.js
+++ semana10/labex-app/src/pages/TripDetailPage.js
@@ -1,8 +1,29 @@
+import axios from 'axios'
+import {useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
 import {goToAdminHomePage} from '../routes/coordinator'
 
 function TripDetailPage() {
     const history = useHistory()
+
+    useEffect(() => {
+        getTripDetail()
+    },[])
+
+    const getTripDetail = async(id) => {
+        const token = window.localStorage.getItem('token')
+
+        try{
+            const res = await axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/trip/${id}`, {
+                headers: {
+                    auth: token
+                }
+            })
+            console.log(res)
+        }catch(error){
+            console.log(error)
+        }
+    }
     
     return (
         <div>
