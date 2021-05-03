76b195cd3c5c7a91f4de1e0d1e7e5478631d56c1
Thiago Jatobá <tmjatob@gmail.com>
2021-05-02 21:11:57 -0300

    [feat] login, cadastro, lista de posts e caminho pra tela de detalhes implementados

--- semana12/labeddit-jatoba/src/pages/PostDetailPage/PostDetail.js
+++ semana12/labeddit-jatoba/src/pages/PostDetailPage/PostDetail.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { BASE_URL } from '../../constants/url'
 import useProtectedPage from '../../hooks/useProtectedPage'
+import useRequesteData from '../../hooks/useRequestData'
 
 const PostDetail = () => {
     useProtectedPage()
+    const params = useParams()
+    const detail = useRequesteData({}, `${BASE_URL}/posts/${params.id}`)
+    console.log(detail)
+
     return (
         <div>
-            <h1>PostDetail</h1>
+            <h1>{detail && detail.text}</h1>
         </div>
     )
 }
