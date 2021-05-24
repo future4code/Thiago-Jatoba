2539c463a5d3e90298d7d685274f3a53013015b8
Thiago Jatobá <tmjatob@gmail.com>
2021-05-24 14:23:55 -0300

    [feat] get users by cpf

--- semana15/labeBank-sistema-bancario/src/index.ts
+++ semana15/labeBank-sistema-bancario/src/index.ts
@@ -7,25 +7,48 @@ const app: Express = express()
 app.use(express.json()) // a função express() inicia a aplicação web com express
 app.use(cors()) // os .use() ativam os módulos de Bodyparser e Cors
 
+type account = {
+    value: number,
+    date: string,
+    description: string
+}
+
 type user = {
     cpf: string,
     name: string,
-    birth: string,
+    birthDate: string,
+    extract: account[]
 }
 
 let users: user[] = [
     {
         cpf:'000.000.000-00',
         name: 'Usuário Teste',
-        birth: "1980-10-09"
+        birthDate: "1980-10-09",
+        extract:[
+            {
+            value: 200,
+            date:'2021-03-20',
+            description: 'Padaria'
+            }
+        ]
     }
 ]
+
 
 app.get('/users', (req: Request, res: Response )=> {
     try{
         const cpf = req.query.cpf
 
+        if (!cpf){
+            throw new Error ('Insert a valid CPF')
+        }
+
         const result = users.filter((user) => user.cpf === cpf)
+
+        if (!result.length){
+            throw new Error ('User not found!')
+        }
 
         res.status(200).send(result)
     } catch(err) {
