c78924433f189428fa8ffdd04e59e3f5311540eb
Thiago Jatobá <tmjatob@gmail.com>
2021-06-14 15:02:49 -0300

    [feat] create AUTH and HASH

--- semana18/cookenu/src/services/authentication.ts
+++ semana18/cookenu/src/services/authentication.ts
@@ -10,5 +10,11 @@ export const generateToken = (input: authenticationData): string =>{
 }
 
 export const getTokenData = (token: string): authenticationData | null =>{
-    
+    try{
+        const { id } = JWT.verify (token, String(process.env.JWT_KEY)) as authenticationData
+        
+        return { id }
+        
+    } catch (error) { return null }
+
 }
